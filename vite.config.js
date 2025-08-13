4b28160bb344ddbe3e422792aea0bfe528501c22
fauzan3l <rumbihgti@gmail.com>
2025-08-14 04:06:22 +0700

    Update vite.config.js to output dist for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist', // output folder jadi dist biar cocok sama default Vercel
+        emptyOutDir: true, // bersihkan folder sebelum build
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
 });
